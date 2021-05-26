64ca063ca6a59a2aa015937ea5c4fbc681855dd3
Oliver Lockwood <olockwood@dalet.com>
2021-05-26 12:08:32 +0100

    Initial commit

---
+++ src/main/java/com/oliverlockwood/mongodbissuedemonstration/MongodbIssueDemonstrationApplication.java
@@ -0,0 +1,13 @@
+package com.oliverlockwood.mongodbissuedemonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbIssueDemonstrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbIssueDemonstrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliverlockwood/mongodbissuedemonstration/MongodbIssueDemonstrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliverlockwood.mongodbissuedemonstration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbIssueDemonstrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
